6bcb9bef9c51037393f1064c0b70320f0ef0f9eb
ATMiura <leha.google@mail.ru>
2020-11-17 00:45:33 +0300

    adaptive 1.2

---
+++ src/js/_parts/_mobileReplace.js
@@ -0,0 +1,14 @@
+$(window).on('load resize', function () {
+	/* проверяет все блоки с data-move и перемещает в блоки в мобилке/десктопе на основе их атрибутов */
+	if ($(window).width() < 768) {
+		$('[data-move]').each(function () {
+			var dataMoveAttr = $(this).data('move');
+			$(this).prependTo('[data-mobile="' + dataMoveAttr + '"]');
+		});
+	} else if ($(window).width() > 767) {
+		$('[data-move]').each(function () {
+			var dataMoveAttr = $(this).data('move');
+			$(this).prependTo('[data-desktop="' + dataMoveAttr + '"]');
+		});
+	}
+});
