623e3fdc5c8d6488c5ffac862f20444a7afe79a5
Alexey Marchenko <leha.google@mail.ru>
2020-12-08 10:36:55 +0300

    my final

--- src/js/_parts/_mobile.js
+++ src/js/_parts/_mobile.js
@@ -1,4 +1,19 @@
-$(document).on('click touch', '[data-mobile-nav]', function () {
-	$('body').toggleClass('overflow');
-	$(this).parents('.mobile').find('.mobile-content').toggleClass('active');
+$(document).on('click touch', '[data-mobile-nav]:not([data-mobile-nav="post-create"])', function () {
+
+	let dataMobileNav = $(this).data('mobile-nav');
+
+	if($('[data-mobile-nav="'+dataMobileNav+'"]').hasClass('active')){
+		$('body').removeClass('overflow');
+		$(this).removeClass('active');
+		$('.mobile-content').removeClass('active');
+	} else {
+		$('body').addClass('overflow');
+		$(this).addClass('active');
+
+		$('.mobile-content').addClass('active');
+	}
+
+
+	$('.mobile-item[data-mobile]').removeClass('active');
+	$('.mobile-item[data-mobile="'+dataMobileNav+'"]').toggleClass('active');
 });
--- src/js/_parts/_mobileReplace.js
+++ src/js/_parts/_mobileReplace.js
@@ -1,13 +1,15 @@
-$(window).on('load resize', function () {
+$(window).on('load', function () {
 	/* проверяет все блоки с data-move и перемещает в блоки в мобилке/десктопе на основе их атрибутов */
 	if ($(window).width() < 768) {
 		$('[data-move]').each(function () {
 			var dataMoveAttr = $(this).data('move');
+			$('[data-mobile="' + dataMoveAttr + '"]').addClass(dataMoveAttr);
 			$(this).prependTo('[data-mobile="' + dataMoveAttr + '"]');
 		});
 	} else if ($(window).width() > 767) {
 		$('[data-move]').each(function () {
 			var dataMoveAttr = $(this).data('move');
+
 			$(this).prependTo('[data-desktop="' + dataMoveAttr + '"]');
 		});
 	}
